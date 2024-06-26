aa53ff2901e99bb6b987b58adb03bea526f55b08
Kikozy <yuexuanxuanw@outlook.com>
2024-06-26 18:13:10 +0800

    <add> 接口和跳转测试

---
+++ app/about/page.tsx
@@ -0,0 +1,28 @@
+import "./about.scss"
+
+async function getData() {
+	const res = await fetch(
+		"https://mock.presstime.cn/mock/667bd5d0025ec67cd8302d16/example/getList"
+	)
+	return res
+}
+
+export default async function About() {
+	const response = await (await getData()).json()
+	const list: Array<any> = response.data
+	return (
+		<>
+			<h1>About</h1>
+			<div className="card-list">
+				{list.map((item, index) => (
+					<div className="card-item">
+						<p className="title">{item.title}</p>
+						<p className="author">{item.author}</p>
+						<p className="time">{item.time}</p>
+						<div className="content">{item.content}</div>
+					</div>
+				))}
+			</div>
+		</>
+	)
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,53 @@
 "use client"
-import { useContext } from "react";
-import { AppContext } from "./components/AppContext";
+import { useContext } from "react"
+import { AppContext } from "./components/AppContext"
 import "./page.scss"
+import Link from "next/link"
 
-
-const navList = ['Index', 'Home', 'About']
+const navList = [
+	{ name: "Index", path: "/index" },
+	{ name: "About", path: "/about" },
+	{ name: "Home", path: "/home" },
+]
 
 export default function Home() {
-  console.log('page reload')
-  const { state, dispatch } = useContext(AppContext)
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className={`nav ${state.display ? "" : "nav-hidden"}`}>
-        <button className="nav-display" onClick={() => dispatch({ type: "UPDATE", filed: "display", value: !state.display })}>{state.display ? "hidden" : "show"}</button>
-        <div className="nav-list">
-          {navList.map(
-            (item, index) => <div key={item}>{item}</div>
-          )}
-        </div>
-
-      </div>
-      <h1>{state.navNmae ? state.navNmae : 'Men! What can i say!'}</h1>
-      <button onClick={() => dispatch({
-        type: "UPDATE",
-        filed: "display",
-        value: true
-      })}>点我一下咯</button>
-    </main >
-  );
+	console.log("page reload")
+	const { state, dispatch } = useContext(AppContext)
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<div className={`nav ${state.display ? "" : "nav-hidden"}`}>
+				<button
+					className="nav-display"
+					onClick={() =>
+						dispatch({
+							type: "UPDATE",
+							filed: "display",
+							value: !state.display,
+						})
+					}
+				>
+					{state.display ? "hidden" : "show"}
+				</button>
+				<div className="nav-list">
+					{navList.map((item, index) => (
+						<Link href={item.path} key={item.name}>
+							{item.name + index}
+						</Link>
+					))}
+				</div>
+			</div>
+			<h1>{state.navNmae ? state.navNmae : "Men! What can i say!"}</h1>
+			<button
+				onClick={() =>
+					dispatch({
+						type: "UPDATE",
+						filed: "display",
+						value: true,
+					})
+				}
+			>
+				点我一下咯
+			</button>
+		</main>
+	)
 }
